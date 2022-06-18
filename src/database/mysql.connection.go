8ea5f8459ce74d1a47c1435d06634ede6a3068b8
samithiwat <samithiwat2544@gmail.com>
2022-06-19 03:48:50 +0700

    Create db connection

---
+++ src/database/mysql.connection.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"fmt"
+	"github.com/isd-sgcu/rnkm65-backend/src/config"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"strconv"
+)
+
+func InitDatabase(conf *config.Database) (db *gorm.DB, err error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True", conf.User, conf.Password, conf.Host, strconv.Itoa(conf.Port), conf.Name)
+
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.AutoMigrate()
+	if err != nil {
+		return nil, err
+	}
+
+	return
+}
---
+++ src/database/redis.connection.go
@@ -0,0 +1,15 @@
+package database
+
+import (
+	"github.com/go-redis/redis/v8"
+	"github.com/isd-sgcu/rnkm65-backend/src/config"
+)
+
+func InitRedisConnect(conf *config.Redis) (cache *redis.Client, err error) {
+	cache = redis.NewClient(&redis.Options{
+		Addr: conf.Host,
+		DB:   0,
+	})
+
+	return
+}
