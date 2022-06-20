bc763615fdfae532f6d42146749288e6c343985a
samithiwat <samithiwat2544@gmail.com>
2022-06-21 01:59:49 +0700

    Create user seeder

---
+++ src/database/seeds/1655751437484-user.seed.go
@@ -0,0 +1,34 @@
+package seed
+
+import (
+	"github.com/bxcodec/faker/v3"
+	"github.com/isd-sgcu/rnkm65-backend/src/app/model/user"
+)
+
+func (s Seed) UserSeed1655751437484() error {
+	for i := 0; i < 10; i++ {
+		usr := user.User{
+			Firstname:             faker.FirstName(),
+			Lastname:              faker.LastName(),
+			Nickname:              faker.Name(),
+			StudentID:             faker.Word(),
+			Faculty:               faker.Word(),
+			Year:                  faker.Word(),
+			Phone:                 faker.Phonenumber(),
+			LineID:                faker.Word(),
+			Email:                 faker.Email(),
+			AllergyFood:           faker.Word(),
+			FoodRestriction:       faker.Word(),
+			AllergyMedicine:       faker.Word(),
+			Disease:               faker.Word(),
+			VaccineCertificateUrl: faker.URL(),
+			ImageUrl:              faker.URL(),
+		}
+		err := s.db.Create(&usr).Error
+
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
