792075693927bd34252c8a1665b1ba304166d014
samithiwat <samithiwat2544@gmail.com>
2022-07-23 20:33:36 +0700

    Update proto file

--- src/app/service/user/user.service_test.go
+++ src/app/service/user/user.service_test.go
@@ -23,6 +23,7 @@ import (
 type UserServiceTest struct {
 	suite.Suite
 	User              *user.User
+	UpdateUser        *user.User
 	UserDto           *proto.User
 	CreateUserReqMock *proto.CreateUserRequest
 	UpdateUserReqMock *proto.UpdateUserRequest
@@ -102,26 +103,32 @@ func (t *UserServiceTest) SetupTest() {
 	}
 
 	t.UpdateUserReqMock = &proto.UpdateUserRequest{
-		User: &proto.User{
-			Id:              t.User.ID.String(),
-			Title:           t.User.Title,
-			Firstname:       t.User.Firstname,
-			Lastname:        t.User.Lastname,
-			Nickname:        t.User.Nickname,
-			StudentID:       t.User.StudentID,
-			Faculty:         t.User.Faculty,
-			Year:            t.User.Year,
-			Phone:           t.User.Phone,
-			LineID:          t.User.LineID,
-			Email:           t.User.Email,
-			AllergyFood:     t.User.AllergyFood,
-			FoodRestriction: t.User.FoodRestriction,
-			AllergyMedicine: t.User.AllergyMedicine,
-			Disease:         t.User.Disease,
-			CanSelectBaan:   *t.User.CanSelectBaan,
-			IsVerify:        *t.User.IsVerify,
-			GroupId:         t.User.GroupID.String(),
-		},
+		Id:              t.User.ID.String(),
+		Title:           t.User.Title,
+		Firstname:       t.User.Firstname,
+		Lastname:        t.User.Lastname,
+		Nickname:        t.User.Nickname,
+		Phone:           t.User.Phone,
+		LineID:          t.User.LineID,
+		Email:           t.User.Email,
+		AllergyFood:     t.User.AllergyFood,
+		FoodRestriction: t.User.FoodRestriction,
+		AllergyMedicine: t.User.AllergyMedicine,
+		Disease:         t.User.Disease,
+	}
+
+	t.UpdateUser = &user.User{
+		Title:           t.User.Title,
+		Firstname:       t.User.Firstname,
+		Lastname:        t.User.Lastname,
+		Nickname:        t.User.Nickname,
+		Phone:           t.User.Phone,
+		LineID:          t.User.LineID,
+		Email:           t.User.Email,
+		AllergyFood:     t.User.AllergyFood,
+		FoodRestriction: t.User.FoodRestriction,
+		AllergyMedicine: t.User.AllergyMedicine,
+		Disease:         t.User.Disease,
 	}
 }
 
@@ -306,12 +313,9 @@ func (t *UserServiceTest) TestVerifyNotFound() {
 func (t *UserServiceTest) TestUpdateSuccess() {
 	want := &proto.UpdateUserResponse{User: t.UserDto}
 
-	userIn := *t.User
-	userIn.IsVerify = nil
-
 	repo := &mock.RepositoryMock{}
 
-	repo.On("Update", t.User.ID.String(), &userIn).Return(t.User, nil)
+	repo.On("Update", t.User.ID.String(), t.UpdateUser).Return(t.User, nil)
 
 	fileSrv := &fMock.ServiceMock{}
 
@@ -323,11 +327,8 @@ func (t *UserServiceTest) TestUpdateSuccess() {
 }
 
 func (t *UserServiceTest) TestUpdateNotFound() {
-	userIn := *t.User
-	userIn.IsVerify = nil
-
 	repo := &mock.RepositoryMock{}
-	repo.On("Update", t.User.ID.String(), &userIn).Return(nil, errors.New("Not found user"))
+	repo.On("Update", t.User.ID.String(), t.UpdateUser).Return(nil, errors.New("Not found user"))
 
 	fileSrv := &fMock.ServiceMock{}
 
@@ -339,28 +340,6 @@ func (t *UserServiceTest) TestUpdateNotFound() {
 	assert.True(t.T(), ok)
 	assert.Nil(t.T(), actual)
 	assert.Equal(t.T(), codes.NotFound, st.Code())
-}
-
-func (t *UserServiceTest) TestUpdateMalformed() {
-	userIn := *t.User
-	userIn.IsVerify = nil
-
-	repo := &mock.RepositoryMock{}
-	repo.On("Update", t.User.ID.String(), userIn).Return(nil, errors.New("Not found user"))
-
-	fileSrv := &fMock.ServiceMock{}
-
-	srv := NewService(repo, fileSrv)
-
-	t.UpdateUserReqMock.User.Id = "abc"
-
-	actual, err := srv.Update(context.Background(), t.UpdateUserReqMock)
-
-	st, ok := status.FromError(err)
-
-	assert.True(t.T(), ok)
-	assert.Nil(t.T(), actual)
-	assert.Equal(t.T(), codes.InvalidArgument, st.Code())
 }
 
 func (t *UserServiceTest) TestDeleteSuccess() {
--- src/database/seeds/1658079351033-baan.seed.go
+++ src/database/seeds/1658079351033-baan.seed.go
@@ -2,7 +2,7 @@ package seed
 
 func (s Seed) BaanSeed1658079351033() error {
 	for _, b := range baans {
-		err := s.db.Create(&b).Error
+		err := s.db.Save(&b).Error
 
 		if err != nil {
 			return err
