63676b3720645e75ca08eca96217a36a58bf4476
samithiwat <samithiwat2544@gmail.com>
2022-08-03 16:03:43 +0700

    Update eStamp seeder

--- src/database/seeds/1658584832819-event.seed.go
+++ src/database/seeds/1658584832819-event.seed.go
@@ -1,12 +1,5 @@
 package seed
 
 func (s Seed) EventSeed1658584832819() error {
-	for _, event := range events {
-		err := s.db.Create(&event).Error
-
-		if err != nil {
-			return err
-		}
-	}
-	return nil
+	return s.db.Save(&events).Error
 }
