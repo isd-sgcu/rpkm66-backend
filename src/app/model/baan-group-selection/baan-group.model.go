9d8ef16d68c80c2b8cfd3dfa16b9ad4f4efde3ff
ImSoZRious <pannawichtata@gmail.com>
2023-07-01 09:56:31 +0700

    fix: db invalid type

--- src/app/model/baan-group-selection/baan-group.model.go
+++ src/app/model/baan-group-selection/baan-group.model.go
@@ -1,9 +1,10 @@
 package baan_group_selection
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 	"github.com/isd-sgcu/rpkm66-backend/src/app/model/baan"
-	"time"
 )
 
 type BaanGroupSelection struct {
@@ -11,6 +12,6 @@ type BaanGroupSelection struct {
 	BaanID    *uuid.UUID `gorm:"primaryKey"`
 	GroupID   *uuid.UUID `gorm:"primaryKey"`
 	Order     int        `json:"order" gorm:"type:tinyint"`
-	CreatedAt time.Time  `json:"created_at" gorm:"type:datetime;autoCreateTime:nano"`
-	UpdatedAt time.Time  `json:"updated_at" gorm:"type:datetime;autoUpdateTime:nano"`
+	CreatedAt time.Time  `json:"created_at" gorm:"type:timestamp;autoCreateTime:nano"`
+	UpdatedAt time.Time  `json:"updated_at" gorm:"type:timestamp;autoUptimestamp:nano"`
 }
--- src/app/model/common.model.go
+++ src/app/model/common.model.go
@@ -1,16 +1,17 @@
 package model
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 	"gorm.io/gorm"
-	"time"
 )
 
 type Base struct {
 	ID        uuid.UUID      `json:"id" gorm:"primary_key"`
-	CreatedAt time.Time      `json:"created_at" gorm:"type:datetime;autoCreateTime:nano"`
-	UpdatedAt time.Time      `json:"updated_at" gorm:"type:datetime;autoUpdateTime:nano"`
-	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index;type:datetime"`
+	CreatedAt time.Time      `json:"created_at" gorm:"type:timestamp;autoCreateTime:nano"`
+	UpdatedAt time.Time      `json:"updated_at" gorm:"type:timestamp;autoUptimestamp:nano"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index;type:timestamp"`
 }
 
 func (m *Base) BeforeCreate(_ *gorm.DB) error {
